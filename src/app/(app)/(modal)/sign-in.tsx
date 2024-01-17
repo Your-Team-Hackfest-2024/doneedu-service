1ac06cc8fbc4181bd92d9c0ecb51a8a891239cd7
Muhammad Ismail <78538191+mail2418@users.noreply.github.com>
2024-01-17 21:43:17 +0700

    Delete src/app directory

--- src/app/(app)/(modal)/sign-in.tsx
+++
@@ -1,5 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-
-export default function SignInModal() {
-  return <AuthForm variant="signin" />;
-}
--- src/app/(app)/payment.tsx
+++
@@ -1,8 +0,0 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Paragraph } from 'tamagui';
-
-export default function PaymentScreen() {
-  const { donationId } = useLocalSearchParams();
-
-  return <Paragraph>Donation ID: {donationId}</Paragraph>;
-}
